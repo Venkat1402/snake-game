1438ee38984e364e13ab60315c04ca3f10a05afd
venkat-katikam <venkat140299@gmail.com>
2023-09-11 23:49:33 +0530

    Updtaed mobile screen buttons

--- input.js
+++ input.js
@@ -22,6 +22,35 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
+function arrowUpClicked() {
+  if (lastInputDirection.y !== 0) return; // If you are already moving up or down you cannot use up or down keys
+  inputDirection = { x: 0, y: -1 };
+  return;
+}
+
+function arrowDownClicked() {
+  if (lastInputDirection.y !== 0) return; // If you are already moving up or down you cannot use up or down keys
+  inputDirection = { x: 0, y: 1 };
+  return;
+}
+
+function arrowLeftClicked() {
+  if (lastInputDirection.x !== 0) return; // If you are already moving left or right you cannot use left or right keys
+  inputDirection = { x: -1, y: 0 };
+  return;
+}
+
+function arrowRightClicked() {
+  if (lastInputDirection.x !== 0) return; // If you are already moving left or right you cannot use left or right keys
+  inputDirection = { x: 1, y: 0 };
+  return;
+}
+
+document.getElementById("arrowUp").onclick = arrowUpClicked;
+document.getElementById("arrowDown").onclick = arrowDownClicked;
+document.getElementById("arrowLeft").onclick = arrowLeftClicked;
+document.getElementById("arrowRight").onclick = arrowRightClicked;
+
 export function getInputDirection() {
   lastInputDirection = inputDirection;
   return inputDirection;
